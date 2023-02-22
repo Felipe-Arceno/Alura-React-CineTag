db53d8dd92e54b9af3ed41d537e253972abafccc
Felipe Arceno <arceno65@gmail.com>
2023-02-21 22:28:48 -0300

    Part 3: Footer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import Header from "../../components/Header";
 import { Fragment } from "react";
+import Footer from "../../components/Footer";
 
 const Home = () => {
   return (
     <Fragment>
       <Header />
       <h1>Olá mundo! Estou numa página nova</h1>
+      <Footer />
     </Fragment>
   );
 };
