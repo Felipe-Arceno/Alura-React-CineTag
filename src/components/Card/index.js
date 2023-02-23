999db23d7e949ddfe20d413f0db1a72076dbb848
Felipe Arceno <arceno65@gmail.com>
2023-02-22 21:16:26 -0300

    Part 7: Favoritos

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,25 @@
+import { useFavoriteContext } from "contextos/Favorites";
 import styles from "./Card.module.css";
 import iconHeartFilled from "./iconHeartFilled.png";
 import iconHeart from "./iconHeartNotFilled.png";
 
 const Card = ({ id, title, cover }) => {
+  const { favorite, addFavorite } = useFavoriteContext();
+  const isFavorite = favorite.some((fav) => fav.id === id);
+  const icon = isFavorite ? iconHeartFilled : iconHeart;
+
   return (
     <div className={styles.container}>
       <img src={cover} alt={title} className={styles.cover} />
       <h2>{title}</h2>
-      <img src={iconHeart} alt="Favoritar Filme" className={styles.favorite} />
+      <img
+        src={icon}
+        alt="Favoritar Filme"
+        className={styles.favorite}
+        onClick={() => {
+          addFavorite({ id, title, cover });
+        }}
+      />
     </div>
   );
 };
